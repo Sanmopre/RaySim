3b1387b51798d7aae458ac81d1bc5228c2c20dd7
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2024-11-26 19:41:30 +0100

    Physics box2d init

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,1 +1,18 @@
 #include "physics.h"
+
+ray_engine::Physics::Physics()
+{
+    worldDef_ = b2DefaultWorldDef();
+    worldDef_.gravity = b2Vec2{0.0f, -9.81f};
+    worldId_ = b2CreateWorld(&worldDef_);
+}
+
+ray_engine::Physics::~Physics()
+{
+    for (const auto& bodyId : bodiesVector_)
+    {
+        b2DestroyBody(bodyId);
+    }
+
+    b2DestroyWorld(worldId_);
+}
--- src/physics.h
+++ src/physics.h
@@ -1,1 +1,22 @@
 #pragma once
+
+// box2d
+#include <vector>
+
+#include "box2d/box2d.h"
+
+namespace ray_engine
+{
+class Physics
+{
+public:
+
+    Physics();
+    ~Physics();
+
+private:
+    b2WorldDef worldDef_;
+    b2WorldId worldId_;
+    std::vector<b2BodyId> bodiesVector_;
+};
+}
